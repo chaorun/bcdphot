ae8656fbf1ab70f0e78ed69a6cf2ea7d81d73f99
John Livingston <jckhnk@yahoo.com>
2014-03-21 14:24:08 -0700

    testing astropy.wcs

--- bcd_list.py
+++ bcd_list.py
@@ -4,7 +4,8 @@ import os
 import sys
 import pyfits
 # import wcslib
-import pywcs
+# import pywcs
+from astropy import wcs
 import itertools
 import numpy as np
 import simplejson as json
@@ -24,9 +25,11 @@ def source_in_image(argslist):
 	ra, dec, fitsfile = argslist
 	hdr = pyfits.getheader(fitsfile)
 	# wcs = wcslib.WcsProjection(hdr)
-	wcs = pywcs.WCS(hdr)
+	# wcs = pywcs.WCS(hdr)
+	w = wcs.WCS(hdr)
 	# x, y = wcs.toPixel(ra, dec, rnd=False)
-	x, y = map(lambda x: x[0], wcs.wcs_sky2pix(ra, dec, 1))
+	# x, y = map(lambda x: x[0], wcs.wcs_sky2pix(ra, dec, 1))
+	x, y = map(lambda x: x[0], w.world_sky2pix(ra, dec, 1))
 	x_max = hdr['NAXIS1']
 	y_max = hdr['NAXIS2']
 	if x >= 0 and x < x_max and y >= 0 and y < y_max:
