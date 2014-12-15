8d2257d4a709d41ba6c50a633905a216967d7a42
John Livingston <jckhnk@yahoo.com>
2014-12-15 11:52:08 -0800

    changes to structure of input setup.yaml file, moved output directory setup to new util.py function setup_output_dirs(), some general code cleanup

--- run.py
+++ run.py
@@ -2,11 +2,10 @@
 
 import os
 import sys
-import glob
 import multiprocessing
 import simplejson as json
 import yaml
-from util import find_files, mkdirs, get_bcd_subset
+from util import find_files, setup_output_dirs
 from bcd_list import get_bcd_list
 from bcd_list import map_bcd_sources
 from bcd_phot import get_bcd_phot 
@@ -22,162 +21,14 @@ ncpus = multiprocessing.cpu_count()
 pool = multiprocessing.Pool(processes=ncpus)
 print "using %i CPUs" % ncpus
 
-# read in the YAML setup file containing the list of dicts, where
-# each dict corresponds to a region and contains the AOR numbers
-# and filepaths containing the list of RA/Dec for that region, as well
-# as some parameters such as whether or not the data are HDR mode, and
-# whether or not to use the *_cbcd.fits files or the *_bcd.fits files.
 infile = sys.argv[1]
 setup = yaml.load(open(infile))
 print('reading input file: '+infile)
 
-regions = setup['regions']
-params = setup['params']
+is_hdr = setup['params']['hdr']
+out_dir = setup['params']['out_dir'].rstrip('/')
 
-# check to see whether to use CBCD or BCD files 
-# (and consequently CBUNC or BUNC files)
-if params['cbcd']:
-	bcd_suffix = '*_cbcd.fits'
-	unc_suffix = '*_cbunc.fits'
-else:
-	bcd_suffix = '*_bcd.fits'
-	unc_suffix = '*_bunc.fits'
-
-# check to see whether the data are HDR mode or not. if they are then
-# there will be additional directory structure in the output directory.
-is_hdr = params['hdr']
-
-# get the path to the IDL executable from the params dict
-idl_path = params['idl_path']
-
-# get the number corresponding to the maximum coverage of any source in
-# the input source lists. an unnecessarily large value will make the 
-# pipeline slower than it has to be, so this value should be set to the
-# appropriate value depending on the observing strategy. if the value is
-# too low then the pipeline will not take advantage of additional data
-# available for some sources.
-max_cov = params['max_cov']
-
-# get the minimum SNR value for an individual measurement to be kept
-min_snr = params['min_snr']
-
-# get the maximum distance from input RA/Dec to centroid position [arcsec]
-max_dist = params['max_dist']
-
-# this is the master project directory containing the data, input file,
-# and subdirectory containing the RA/Dec source list files
-data_dir = params['data_dir'].rstrip('/')
-print('data directory: '+data_dir)
-
-# this is the name of the output dir for pipeline output and temp files
-out_dir = params['out_dir'].rstrip('/')
-mkdirs(out_dir)
-print('output directory: '+out_dir)
-
-# if out_dir not empty, assume bcd_dict.json and unc_dict.json exist 
-# and are valid (this can save time if running the same region
-# more than once, i.e. during testing)
-if len(os.listdir(out_dir)) is 0:
-
-	# create a dictionary with BCD filenames as the keys, full paths as values
-	print('creating setup files in output directory...')
-	bcd_paths = list(find_files(data_dir, bcd_suffix))
-	bcd_dict = {i.split('/')[-1]:i for i in bcd_paths}
-	with open(out_dir+'/bcd_dict.json','w') as w:
-		json.dump(bcd_dict, w, indent=' '*4)
-	# do the same for UNC files
-	unc_paths = list(find_files(data_dir, unc_suffix))
-	unc_dict = {i.split('/')[-1]:i for i in unc_paths}
-	with open(out_dir+'/unc_dict.json','w') as w:
-		json.dump(unc_dict, w, indent=' '*4)
-	# do the same for MSK files
-	msk_paths = list(find_files(data_dir, '*_bimsk.fits'))
-	msk_dict = {i.split('/')[-1]:i for i in msk_paths}
-	with open(out_dir+'/msk_dict.json','w') as w:
-		json.dump(msk_dict, w, indent=' '*4)
-
-else:
-
-	# read in existing bcd_dict.json, unc_dict.json
-	bcd_dict = json.load(open(out_dir+'/bcd_dict.json'))
-	unc_dict = json.load(open(out_dir+'/unc_dict.json'))
-	msk_dict = json.load(open(out_dir+'/msk_dict.json'))
-
-# loop through the source lists (radecfiles) and create output directory
-# structure and metadata files used throughout the rest of the pipeline
-print('creating directory structure and metadata in output directory...')
-work_dirs = []
-for region in regions:
-
-	name = region['name']
-	radecfiles = region['radec']
-	aors = region['aors']
-
-	for radecfile in radecfiles:
-		f = radecfile.split('/')[-1]
-		if is_hdr:
-			ch, hdr = f.split('_')[1:3]
-			work_dir = '/'.join([out_dir,name,ch,hdr])
-		else:
-			ch = f.split('_')[1]
-			work_dir = '/'.join([out_dir,name,ch])
-		mkdirs(work_dir)
-		work_dirs.append(work_dir)
-
-		# make subset bcd_dict/unc_dict in each work_dir
-		if is_hdr:
-			bcd_paths_subset = get_bcd_subset(bcd_dict, aors, ch, hdr)
-			unc_paths_subset = get_bcd_subset(unc_dict, aors, ch, hdr)
-			msk_paths_subset = get_bcd_subset(msk_dict, aors, ch, hdr)
-		else:
-			bcd_paths_subset = get_bcd_subset(bcd_dict, aors, ch)
-			unc_paths_subset = get_bcd_subset(unc_dict, aors, ch)
-			msk_paths_subset = get_bcd_subset(msk_dict, aors, ch)
-
-		bcd_dict_subset = {i.split('/')[-1]:i for i in bcd_paths_subset}
-		unc_dict_subset = {i.split('/')[-1]:i for i in unc_paths_subset}
-		msk_dict_subset = {i.split('/')[-1]:i for i in msk_paths_subset}
-
-		bcd_dict_path = work_dir+'/bcd_dict.json'
-		unc_dict_path = work_dir+'/unc_dict.json'
-		msk_dict_path = work_dir+'/msk_dict.json'
-
-		with open(bcd_dict_path,'w') as w:
-			json.dump(bcd_dict_subset, w, indent=' '*4)
-		print('created: '+bcd_dict_path)
-
-		with open(unc_dict_path,'w') as w:
-			json.dump(unc_dict_subset, w, indent=' '*4)
-		print('created: '+unc_dict_path)
-
-		with open(msk_dict_path,'w') as w:
-			json.dump(msk_dict_subset, w, indent=' '*4)
-		print('created: '+msk_dict_path)
-
-		metadata = {'name':name, 'data_dir':data_dir, 'work_dir':work_dir,
-			'out_dir':out_dir, 'radecfile':radecfile,
-			'bcd_dict_path':bcd_dict_path,
-			'unc_dict_path':unc_dict_path,
-			'msk_dict_path':msk_dict_path,
-			'aors':aors, 'channel':ch, 
-			'cbcd':params['cbcd'], 'mask':params['mask'],
-			'idl_path': idl_path, 'max_cov': max_cov,
-			'min_snr':min_snr, 'max_dist':max_dist,
-			'sigma_clip':params['sigma_clip'],
-			'centroid':params['centroid']
-			}
-		if is_hdr:
-			if ch == '1':
-				metadata['long_cutoff'] = params['long_cutoff_ch1']
-				metadata['short_cutoff'] = params['short_cutoff_ch1']
-			elif ch == '2':
-				metadata['long_cutoff'] = params['long_cutoff_ch2']
-				metadata['short_cutoff'] = params['short_cutoff_ch2']
-			metadata['hdr'] = hdr
-		metadata_path = work_dir+'/metadata.json'
-		with open(metadata_path,'w') as w:
-			json.dump(metadata, w, indent=' '*4)
-		print('created: '+metadata_path)
+setup_output_dirs(setup)
 
 # if bcd_list.json files exist, assume valid and don't run get_bcd_list
 if len(list(find_files(out_dir, 'bcd_list.json'))) is 0:
@@ -185,9 +36,9 @@ if len(list(find_files(out_dir, 'bcd_list.json'))) is 0:
 	# loop through the region/channel/hdr file structure just created and 
 	# read the metadata for each, then call get_bcd_list(meta)
 	print('associating input sources with BCDs for:')
-	for work_dir in work_dirs:
-		print(work_dir)
-		metadata = json.load(open(work_dir+'/metadata.json'))
+	for metafile in find_files(out_dir, 'metadata.json'):
+		print(metafile)
+		metadata = json.load(open(metafile))
 		get_bcd_list(metadata)
 
 # if source_list.json files exist, assume valid and don't run map_bcd_sources
@@ -208,9 +59,10 @@ pool.map(get_bcd_phot, filepaths)
 
 # cull bad individual measurements from the groups of measurements
 # of each source using SNR and proximity cutoffs
-# print('culling bad measurements...')
-# filepaths = list(find_files(out_dir, 'phot_groups.json'))
-# pool.map(cull_bad_measurements, filepaths)
+if setup['params']['snr_dist_cull']:
+	print('culling bad measurements...')
+	filepaths = list(find_files(out_dir, 'phot_groups.json'))
+	pool.map(cull_bad_measurements, filepaths)
 
 # apply array location correction to phot_groups.json files
 print('applying array location correction...')
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import fnmatch
 import numpy as np
 from functools import partial
@@ -240,3 +241,140 @@ def ordinary_least_squares(y, X, intercept=False):
 		return np.dot( np.dot( 1. / ( np.dot(X.T, X) ), X.T), y )
 	else:
 		return np.dot( np.dot( np.linalg.inv( np.dot(X.T, X) ), X.T), y )
+
+
+def setup_output_dirs(setup):
+
+	regions = setup['regions']
+	params = setup['params']
+
+	# check to see whether to use CBCD or BCD files 
+	# (and consequently CBUNC or BUNC files)
+	if params['cbcd']:
+		bcd_suffix = '*_cbcd.fits'
+		unc_suffix = '*_cbunc.fits'
+	else:
+		bcd_suffix = '*_bcd.fits'
+		unc_suffix = '*_bunc.fits'
+
+	# check to see whether the data are HDR mode or not. if they are then
+	# there will be additional directory structure in the output directory.
+	is_hdr = params['hdr']
+
+	if setup['params']['snr_dist_cull']:
+		# get the minimum SNR value for an individual measurement to be kept
+		min_snr = params['min_snr']
+		# get the maximum distance from input RA/Dec to centroid position [arcsec]
+		max_dist = params['max_dist']
+
+	# this is the master project directory containing the data, input file,
+	# and subdirectory containing the RA/Dec source list files
+	data_dir = params['data_dir'].rstrip('/')
+	print('data directory: '+data_dir)
+
+	# this is the name of the output dir for pipeline output and temp files
+	out_dir = params['out_dir'].rstrip('/')
+	mkdirs(out_dir)
+	print('output directory: '+out_dir)
+
+	# create a dictionary with BCD filenames as the keys, full paths as values
+	print('creating setup files in output directory...')
+	bcd_paths = list(find_files(data_dir, bcd_suffix))
+	bcd_dict = {i.split('/')[-1]:i for i in bcd_paths}
+	with open(out_dir+'/bcd_dict.json','w') as w:
+		json.dump(bcd_dict, w, indent=' '*4)
+	# do the same for UNC files
+	unc_paths = list(find_files(data_dir, unc_suffix))
+	unc_dict = {i.split('/')[-1]:i for i in unc_paths}
+	with open(out_dir+'/unc_dict.json','w') as w:
+		json.dump(unc_dict, w, indent=' '*4)
+	# do the same for MSK files
+	msk_paths = list(find_files(data_dir, '*_bimsk.fits'))
+	msk_dict = {i.split('/')[-1]:i for i in msk_paths}
+	with open(out_dir+'/msk_dict.json','w') as w:
+		json.dump(msk_dict, w, indent=' '*4)
+
+	# loop through the source lists (radecfiles) and create output directory
+	# structure and metadata files used throughout the rest of the pipeline
+	print('creating directory structure and metadata in output directory...')
+	for region in regions:
+
+		name = region['name']
+		radecfiles = region['radec']
+		aors = region['aors']
+
+		for radec in radecfiles:
+			f = radec['filepath']
+			if is_hdr:
+				ch = str(radec['channel'])
+				hdr = radec['hdr']
+				work_dir = '/'.join([out_dir, name, ch, hdr])
+				mkdirs(work_dir)
+			else:
+				ch = str(radec['channel'])
+				work_dir = '/'.join([out_dir, name, ch])
+				mkdirs(work_dir)
+
+			# make subset bcd_dict/unc_dict in each work_dir
+			if is_hdr:
+				bcd_paths_subset = get_bcd_subset(bcd_dict, aors, ch, hdr)
+				unc_paths_subset = get_bcd_subset(unc_dict, aors, ch, hdr)
+				msk_paths_subset = get_bcd_subset(msk_dict, aors, ch, hdr)
+			else:
+				bcd_paths_subset = get_bcd_subset(bcd_dict, aors, ch)
+				unc_paths_subset = get_bcd_subset(unc_dict, aors, ch)
+				msk_paths_subset = get_bcd_subset(msk_dict, aors, ch)
+
+			bcd_dict_subset = {i.split('/')[-1]:i for i in bcd_paths_subset}
+			unc_dict_subset = {i.split('/')[-1]:i for i in unc_paths_subset}
+			msk_dict_subset = {i.split('/')[-1]:i for i in msk_paths_subset}
+
+			bcd_dict_path = work_dir+'/bcd_dict.json'
+			unc_dict_path = work_dir+'/unc_dict.json'
+			msk_dict_path = work_dir+'/msk_dict.json'
+
+			with open(bcd_dict_path,'w') as w:
+				json.dump(bcd_dict_subset, w, indent=' '*4)
+			print('created: '+bcd_dict_path)
+
+			with open(unc_dict_path,'w') as w:
+				json.dump(unc_dict_subset, w, indent=' '*4)
+			print('created: '+unc_dict_path)
+
+			with open(msk_dict_path,'w') as w:
+				json.dump(msk_dict_subset, w, indent=' '*4)
+			print('created: '+msk_dict_path)
+
+			metadata = {
+				'name': name,
+				'data_dir':data_dir,
+				'work_dir':work_dir,
+				'out_dir': out_dir, 
+				'radecfile': radecfile,
+				'bcd_dict_path': bcd_dict_path,
+				'unc_dict_path': unc_dict_path,
+				'msk_dict_path': msk_dict_path,
+				'aors':aors, 
+				'channel':ch, 
+				'cbcd': params['cbcd'],
+				'mask': params['mask'],
+				'idl_path': params['idl_path'], 
+				'max_cov': params['max_cov'],
+				'sigma_clip':params['sigma_clip'],
+				'centroid':params['centroid']
+				}
+			if setup['params']['snr_dist_cull']:
+				metadata['min_snr'] = min_snr
+				metadata['max_dist'] = max_dist
+			if is_hdr:
+				if ch == '1':
+					metadata['long_cutoff'] = params['long_cutoff_ch1']
+					metadata['short_cutoff'] = params['short_cutoff_ch1']
+				elif ch == '2':
+					metadata['long_cutoff'] = params['long_cutoff_ch2']
+					metadata['short_cutoff'] = params['short_cutoff_ch2']
+				metadata['hdr'] = hdr
+			metadata_path = work_dir+'/metadata.json'
+			with open(metadata_path,'w') as w:
+				json.dump(metadata, w, indent=' '*4)
+			print('created: '+metadata_path)
