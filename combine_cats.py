6de0553b79bf569f41c7564c155022cbd3af3d62
John Livingston <jckhnk@yahoo.com>
2015-06-04 15:58:21 -0700

    upgraded combine_cats.py to work with the new median catalog format

--- combine_cats.py
+++ combine_cats.py
@@ -8,8 +8,13 @@ from util import spherematch, spz_jy_to_mags, get_complement
 import matplotlib.pyplot as plt
 
 
-base_dir = '/Users/jlivings/data/spikes/railspikes_database_prep'
-cat_dir = os.path.join(base_dir, 'complete_cats')
+cat_dir = '/Users/jlivings/data/spikes/railspikes_database_prep/complete_cats'
+out_dir = '/Users/jlivings/data/spikes/railspikes_database_prep/combined'
+try:
+	os.mkdir(out_dir)
+except:
+	pass
+
 
 # step 1: combine individual tile catalogs
 # ------------------------------------------
@@ -36,8 +41,8 @@ for fp in filepaths[1:]:
 
 	# keep track of how many got matched and what their (mean) coordinates are
 	matched += idx1.size
-	new_ra = np.c_[df1[['ra_1', 'ra_2']].loc[idx1].values, df2[['ra_1', 'ra_2']].loc[idx2].values].mean(axis=1)
-	new_dec = np.c_[df1[['dec_1', 'dec_2']].loc[idx1].values, df2[['dec_1', 'dec_2']].loc[idx2].values].mean(axis=1)
+	new_ra = np.nanmean( np.c_[df1[['ra_1', 'ra_2']].loc[idx1].values, df2[['ra_1', 'ra_2']].loc[idx2].values], axis=1)
+	new_dec = np.nanmean( np.c_[df1[['dec_1', 'dec_2']].loc[idx1].values, df2[['dec_1', 'dec_2']].loc[idx2].values], axis=1)
 	matched_ra = np.append(matched_ra, new_ra)
 	matched_dec = np.append(matched_dec, new_dec)
 	# combine the matched sources into single sources before adding
@@ -52,9 +57,13 @@ for fp in filepaths[1:]:
 	# i.e. unc_phot ** 2 = unc_tot ** 2 - unc_sys ** 2
 	concat_mean.unc_1 /= np.sqrt(2)
 	concat_mean.unc_2 /= np.sqrt(2)
+	concat_mean.unc_med_1 /= np.sqrt(2)
+	concat_mean.unc_med_2 /= np.sqrt(2)
 	concat_sum = concat.groupby(concat.index).sum()
 	concat_mean.n_obs_1 = concat_sum.n_obs_1
 	concat_mean.n_obs_2 = concat_sum.n_obs_2
+	concat_mean.n_obs_clip_1 = concat_sum.n_obs_clip_1
+	concat_mean.n_obs_clip_2 = concat_sum.n_obs_clip_2
 	# update df1[idx1] with these new values
 	not_idx1 = get_complement(idx1, ra1.size)
 	# data = np.r_[df1.loc[not_idx1].values, concat_mean.values]
@@ -75,36 +84,42 @@ print("{} matched sources combined into single measurements".format(matched))
 # prepare output dataset
 ra = df1[['ra_1', 'ra_2']].mean(axis=1)
 dec = df1[['dec_1', 'dec_2']].mean(axis=1)
-i1 = spz_jy_to_mags(df1['flux_1'] * 1e-3, 1)
-i1unc = 1.08 * df1['unc_1'] / df1['flux_1']
-i2 = spz_jy_to_mags(df1['flux_2'] * 1e-3, 2)
-i2unc = 1.08 * df1['unc_2'] / df1['flux_2']
+# i1 = spz_jy_to_mags(df1['flux_1'] * 1e-3, 1)
+# i1unc = 1.08 * df1['unc_1'] / df1['flux_1']
+# i2 = spz_jy_to_mags(df1['flux_2'] * 1e-3, 2)
+# i2unc = 1.08 * df1['unc_2'] / df1['flux_2']
+i1 = spz_jy_to_mags(df1['flux_med_1'] * 1e-3, 1)
+i2 = spz_jy_to_mags(df1['flux_med_2'] * 1e-3, 2)
+i1unc = 1.08 * df1['unc_med_1'] / df1['flux_med_1']
+i2unc = 1.08 * df1['unc_med_2'] / df1['flux_med_2']
 df = pd.DataFrame(dict(ra=ra, dec=dec, i1_mag=i1, i1_unc=i1unc, 
 	i2_mag=i2, i2_unc=i2unc, i1_obs=df1['n_obs_1'], i2_obs=df1['n_obs_2']))
 df = df[['ra', 'dec', 'i1_mag', 'i1_unc', 'i2_mag', 'i2_unc', 'i1_obs', 'i2_obs']]
-outpath	= os.path.join(base_dir, 'combined_cats.csv')
-df.to_csv(outpath, index=False)
+outpath	= os.path.join(out_dir, 'combined_cats.csv')
+df.to_csv(outpath, index=False, float_format='%.6f')
+print('created file: '+outpath)
 
 # make plots
 plt.hist2d(ra, dec, bins=180, cmap=plt.cm.hot)
-outpath = os.path.join(base_dir, 'combined_cats.pdf')
+outpath = os.path.join(out_dir, 'combined_cats.pdf')
 plt.savefig(outpath, dpi=120)
 plt.close()
 plt.hist2d(matched_ra, matched_dec, bins=180, cmap=plt.cm.hot)
 xlim, ylim = plt.xlim(), plt.ylim()
-outpath = os.path.join(base_dir, 'tile_overlap_matched_sources.pdf')
+outpath = os.path.join(out_dir, 'tile_overlap_matched_sources.pdf')
 plt.savefig(outpath, dpi=120)
 plt.close()
 plt.hist2d(ra, dec, bins=180, cmap=plt.cm.hot)
 plt.xlim(xlim)
 plt.ylim(ylim)
-outpath = os.path.join(base_dir, 'combined_cats_zoom.pdf')
+outpath = os.path.join(out_dir, 'combined_cats_zoom.pdf')
 plt.savefig(outpath, dpi=120)
 plt.close()
 
+
 # step 2: match to the kic koi ukirtj data
 # ------------------------------------------
-# outpath	= os.path.join(base_dir, 'combined_cats.csv')
+# outpath	= os.path.join(out_dir, 'combined_cats.csv')
 # df = pd.read_csv(outpath)
 kic = pd.read_csv('/Users/jlivings/data/spikes/other_catalogs/kic_kois_ukirtj.csv')
 ra1, dec1 = kic.kic_ra, kic.kic_dec
@@ -117,9 +132,9 @@ kic_matched = kic.loc[idx1]
 df_matched = df.loc[idx2]
 for i in df.columns[2:]:
 	kic_matched[i] = df_matched[i].values
-outpath = os.path.join(base_dir, 'combined_cats+kic_kois_ukirtj.csv')
+outpath = os.path.join(out_dir, 'combined_cats+kic_kois_ukirtj.csv')
 kic_matched.to_csv(outpath, index=False)
-outpath	= os.path.join(base_dir, 'combined_cats_matched_to_kois.pdf')
+outpath	= os.path.join(out_dir, 'combined_cats_matched_to_kois.pdf')
 plt.hexbin(kic_matched.kic_ra, kic_matched.kic_dec)
 plt.savefig(outpath, dpi=120)
 plt.close()
@@ -127,7 +142,7 @@ plt.close()
 # now add two more columns for colors
 kic_matched['i1i2color'] = kic_matched['i1_mag'] - kic_matched['i2_mag']
 kic_matched['ji2color'] = kic_matched['ukirt_j'] - kic_matched['i2_mag']
-outpath = os.path.join(base_dir, 'combined_cats+kic_kois_ukirtj_color.csv')
+outpath = os.path.join(out_dir, 'combined_cats+kic_kois_ukirtj_color.csv')
 kic_matched.to_csv(outpath, index=False)
 
 
@@ -145,9 +160,9 @@ kic_matched = kic.loc[idx1]
 df_matched = df.loc[idx2]
 for i in df.columns[2:]:
 	kic_matched[i] = df_matched[i].values
-outpath = os.path.join(base_dir, 'combined_cats+kic_kobs_ukirtj.csv')
+outpath = os.path.join(out_dir, 'combined_cats+kic_kobs_ukirtj.csv')
 kic_matched.to_csv(outpath, index=False)
-outpath	= os.path.join(base_dir, 'combined_cats_matched_to_kobs.pdf')
+outpath	= os.path.join(out_dir, 'combined_cats_matched_to_kobs.pdf')
 plt.hexbin(kic_matched.kic_ra, kic_matched.kic_dec)
 plt.savefig(outpath, dpi=120)
 plt.close()
@@ -155,7 +170,7 @@ plt.close()
 # now add two more columns for colors
 kic_matched['i1i2color'] = kic_matched['i1_mag'] - kic_matched['i2_mag']
 kic_matched['ji2color'] = kic_matched['ukirt_j'] - kic_matched['i2_mag']
-outpath = os.path.join(base_dir, 'combined_cats+kic_kobs_ukirtj_color.csv')
+outpath = os.path.join(out_dir, 'combined_cats+kic_kobs_ukirtj_color.csv')
 kic_matched.to_csv(outpath, index=False)
 
 
@@ -173,11 +188,26 @@ def plot_simple_pos(ra, dec, outpath):
 	plt.savefig(outpath, dpi=120)
 	plt.close()
 
-kois = pd.read_csv('combined_cats+kic_kois_ukirtj.csv')
-kobs = pd.read_csv('combined_cats+kic_kobs_ukirtj.csv')
+kois = pd.read_csv(os.path.join(out_dir, 'combined_cats+kic_kois_ukirtj_color.csv'))
+kobs = pd.read_csv(os.path.join(out_dir, 'combined_cats+kic_kobs_ukirtj_color.csv'))
 
-outpath	= os.path.join(base_dir, 'KOIs.png')
+outpath	= os.path.join(out_dir, 'KOIs.png')
 plot_simple_pos(kois.kic_ra, kois.kic_dec, outpath)
 
-outpath	= os.path.join(base_dir, 'KOBs.png')
+outpath	= os.path.join(out_dir, 'KOBs.png')
 plot_simple_pos(kobs.kic_ra, kobs.kic_dec, outpath)
+
+
+plt.plot(kois.i1_mag, kois.i1i2color, 'k.', alpha=0.1)
+plt.xlim(9, 15)
+plt.ylim(-0.25,0.25)
+# plt.show()
+plt.savefig(os.path.join(out_dir, 'kois_colormag.png'))
+plt.close()
+
+plt.plot(kobs.i1_mag, kobs.i1i2color, 'k.', alpha=0.1)
+plt.xlim(9, 15)
+plt.ylim(-0.25,0.25)
+# plt.show()
+plt.savefig(os.path.join(out_dir, 'kobs_colormag.png'))
+plt.close()
